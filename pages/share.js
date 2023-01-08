7702b1b4c48551acce363f542bfe6d5e20f38907
devRabbani <dev.golamrabbani@gmail.com>
2023-01-08 20:17:12 +0530

    Fixing share target v3.2

--- pages/share.js
+++ pages/share.js
@@ -1,3 +1,14 @@
 export default function Share() {
-  return <h1>This is SHare {window?.location?.href}</h1>
+  const parsedUrl = new URL(window.location.toString())
+  const title = parsedUrl.searchParams.get('title')
+  const text = parsedUrl.searchParams.get('text')
+  const url = parsedUrl.searchParams.get('url')
+  return (
+    <>
+      <h1>This is SHar</h1>
+      <p>Title : {title}</p>
+      <p>Text : {text}</p>
+      <p>Url : {url}</p>
+    </>
+  )
 }
