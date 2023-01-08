f59d14fd42121a2815673f6dd0a30a063f5888b7
devRabbani <dev.golamrabbani@gmail.com>
2023-01-08 21:25:38 +0530

    Adding final method v4

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRef, useState } from 'react'
 import Loading from '../components/loading'
 import { useAuth } from '../contexts/auth/authContext'
 import { useKeepsList } from '../contexts/keepLists'
@@ -10,6 +11,25 @@ export default function Home() {
 
   const { user } = useAuth()
   const recents = JSON.parse(localStorage.getItem('recents'))
+
+  // Getting share Data
+  const parsedUrl = new URL(window.location.toString())
+  const title = parsedUrl.searchParams.get('title')
+  const text = parsedUrl.searchParams.get('text')
+
+  const [isCopied, setIsCopied] = useState(false)
+
+  const shareRef = useRef()
+
+  // Functions
+  // COpy to clipboard
+  const handleCopy = () => {
+    const value = shareRef.current.innerText
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(value)
+      setIsCopied(true)
+    }
+  }
 
   if (loading) {
     return <Loading text="Getting Data.." />
@@ -22,7 +42,24 @@ export default function Home() {
       </Head>
 
       <div className={`${s.homePage} wrapper`}>
+        {!!title || !!text ? (
+          <>
+            <p className={s.shareP}>Share Data:</p>
+            <div className={s.shareData} ref={shareRef}>
+              {title} <br />
+              {text}
+            </div>
+            <div className={s.btnDiv}>
+              <button onClick={handleCopy}>
+                {isCopied ? 'Copied' : 'Copy'}
+              </button>
+              <Link href="/">Cancel</Link>
+            </div>
+          </>
+        ) : null}
+
         <h1>Welcome {user?.displayName || 'User'}</h1>
+
         <h3 className={s.recentlyViewed}>Recently Viewed</h3>
         {recents?.length ? (
           <div className={s.keepLists}>
