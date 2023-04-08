7445f88722f2b08c7f9cbefa5c3fa5e2c10a7cf7
Golam Rabbani <golamrabbani20014@gmail.com>
2023-04-08 17:31:36 +0530

    Chnaging domain

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 KeepKaro - Now keep and share your important links, notes with friends. With feature like live editing , recently viewed keep, share keep, darkmode, etc
 
-Powered by [CanWeBe](https://canwebe.tech) , Made with mainly NextJs and Firebase
+Powered by [CanWeBe](https://canwebe.in) , Made with mainly NextJs and Firebase
 
-[View Live : KeepKaro](https://keepkaro.canwebe.tech)
+[View Live : KeepKaro](https://keepkaro.canwebe.in)
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default function Document() {
         <meta name="theme-color" content="#d90429" />
         <meta name="color-scheme" content="light dark" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://keepkaro.canwebe.tech" />
+        <meta name="twitter:url" content="https://keepkaro.canwebe.in" />
         <meta name="twitter:title" content="KeepKaro" />
         <meta
           name="twitter:description"
@@ -19,7 +19,7 @@ export default function Document() {
         />
         <meta
           name="twitter:image"
-          content="https://keepkaro.canwebe.tech/logo192.png"
+          content="https://keepkaro.canwebe.in/logo192.png"
         />
         <meta name="twitter:creator" content="@Iamrrk__" />
         <meta property="og:type" content="website" />
@@ -29,10 +29,10 @@ export default function Document() {
           content="Now Keep your all important notes, links and share with your friends"
         />
         <meta property="og:site_name" content="KeepKaro" />
-        <meta property="og:url" content="https://keepkaro.canwebe.tech" />
+        <meta property="og:url" content="https://keepkaro.canwebe.in" />
         <meta
           property="og:image"
-          content="https://keepkaro.canwebe.tech/logo512.png"
+          content="https://keepkaro.canwebe.in/logo512.png"
         />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
